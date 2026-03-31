d5862a6195c6194c7a678055e65095eb577f5ce5
ayushkumarcode <ayushkumargithub@gmail.com>
2026-03-31 05:04:20 +0100

    generate_thesis_figures.py pt2

--- snn-esc50/generate_thesis_figures.py
+++ snn-esc50/generate_thesis_figures.py
@@ -16,3 +16,21 @@ plt.rcParams.update({
 })
 
 OUTDIR = '/Users/kumar/Documents/University/Year3/thesisproject/snn-esc50/paper/figures'
+
+# Colorblind-friendly palette (Wong 2011)
+BLUE = '#0072B2'
+ORANGE = '#E69F00'
+RED = '#D55E00'
+GREEN = '#009E73'
+PURPLE = '#CC79A7'
+GREY = '#999999'
+
+
+# ============================================================
+# FIGURE 1: Pruning-Energy Pareto Curve
+# ============================================================
+def fig1_pruning_pareto():
+    prune_levels = [0, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95]
+    acc =   [57.35, 60.45, 59.15, 60.15, 59.60, 59.85, 59.65, 59.40, 59.65, 58.05, 54.20]
+    energy = [4706,  3059,  2854,  2602,  2372,  2122,  1857,  1624,  1353,  1072,   747]
+
